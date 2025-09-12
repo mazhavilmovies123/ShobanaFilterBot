5c26c6835b6a89838706124c7d8129dda8dd0998
mazhavilmovies123 <muralirokz35@gmail.com>
2025-09-12 14:53:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -175,9 +175,9 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
               [
-                  InlineKeyboardButton(f'Oᴛᴛ Rᴇʟᴇᴀsᴇs Uᴘᴅᴀᴛᴇs'​', url='https://t.me/mazhavilMovies_Updates'),
-                  InlineKeyboardButton(f'Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs', url='https://t.me/MazhavilMoviesLinks'),
-                  InlineKeyboardButton('Bᴏᴛ Uᴘᴅᴀᴛᴇs Gʀᴏᴜᴘ', url='https://t.me/mazhavil_bots')
+                  InlineKeyboardButton(f'ᴏᴛᴛ ᴜᴘᴅᴀᴛᴇs'​', url='https://t.me/mazhavilMovies_Updates'),
+                  InlineKeyboardButton(f'ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ', url='https://t.me/MazhavilMoviesLinks'),
+                  InlineKeyboardButton('Bot support group', url='https://t.me/mazhavil_bots')
          ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -198,10 +198,10 @@ async def start(client, message):
             InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ], [
-             InlineKeyboardButton(f'​Oᴛᴛ Rᴇʟᴇᴀsᴇs Uᴘᴅᴀᴛᴇs'', url='https://t.me/mazhavilMovies_Updates'),
-             InlineKeyboardButton(f'Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs', url='https://t.me/MazhavilMoviesLinks')
+             InlineKeyboardButton(f'​ᴏᴛᴛ ᴜᴘᴅᴀᴛᴇs', url='https://t.me/mazhavilMovies_Updates'),
+             InlineKeyboardButton(f'ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ', url='https://t.me/MazhavilMoviesLinks')
         ],[
-            InlineKeyboardButton('Bᴏᴛ Uᴘᴅᴀᴛᴇs Gʀᴏᴜᴘ', url='https://t.me/mazhavil_bots')
+            InlineKeyboardButton('Bot support group', url='https://t.me/mazhavil_bots')
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_text("ShobanaFilterBot") 
@@ -240,10 +240,10 @@ async def start(client, message):
             InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ], [
-             InlineKeyboardButton(f'Oᴛᴛ Rᴇʟᴇᴀsᴇs Uᴘᴅᴀᴛᴇs​', url='https://t.me/mazhavilMovies_Updates'),
-             InlineKeyboardButton(f'Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs', url='https://t.me/MazhavilMoviesLinks')
+             InlineKeyboardButton(f'​ᴏᴛᴛ ᴜᴘᴅᴀᴛᴇs', url='https://t.me/mazhavilMovies_Updates'),
+             InlineKeyboardButton(f'ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ', url='https://t.me/MazhavilMoviesLinks')
         ],[
-            InlineKeyboardButton('Bᴏᴛ Uᴘᴅᴀᴛᴇs Gʀᴏᴜᴘ', url='https://t.me/mazhavil_bots')
+            InlineKeyboardButton('Bot support group', url='https://t.me/mazhavil_bots')
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
