af8417c810d8b07f3ac28ebfe56f54918420ec49
mazhavilmovies123 <muralirokz35@gmail.com>
2025-10-24 07:36:31 +0530

    Update movie release link in commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -197,7 +197,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('⚓️Oᴛᴛ ᴍᴏᴠɪᴇ sᴛʀᴇᴀᴍɪɴɢ⚓️', url='https://t.me/+NE3VMORHtoU3OWU1')
         ],[
-            InlineKeyboardButton('⚙Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs⚙', url='https://t.me/+eqPQjQe2qGYyODU1')
+            InlineKeyboardButton('⚙Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs⚙', url='https://t.me/+O7J_SlGPgp1iYmE1')
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -232,7 +232,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('⚓️Oᴛᴛ ᴍᴏᴠɪᴇ sᴛʀᴇᴀᴍɪɴɢ⚓️', url='https://t.me/+NE3VMORHtoU3OWU1')
         ],[
-            InlineKeyboardButton('⚙Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs⚙', url='https://t.me/+eqPQjQe2qGYyODU1')
+            InlineKeyboardButton('⚙Lᴀᴛᴇsᴛ Mᴏᴠɪᴇ Rᴇʟᴇᴀsᴇs⚙', url='https://t.me/+O7J_SlGPgp1iYmE1')
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
